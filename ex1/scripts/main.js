48f6ef11150987e9f6e3a862af1b4cfd1a0aa14d
sashimi390 <67630672+sashimi390@users.noreply.github.com>
2020-10-12 10:57:22 +0900

    comp

--- ex1/scripts/main.js
+++ ex1/scripts/main.js
@@ -8,3 +8,29 @@ myImage.onclick = function () {
         myImage.setAttribute('src', 'images//sno2.png');
     }
 }
+
+// Personalized welcome message code
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  if(!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'Snow Man, ' + myName;
+  }
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'Snow Man, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
