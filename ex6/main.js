4151d0b05d07061b6977589fbabeb79d225ffbd4
sashimi390 <67630672+sashimi390@users.noreply.github.com>
2020-11-09 09:01:41 +0900

    eye

--- ex6/main.js
+++ ex6/main.js
@@ -5,17 +5,26 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Looping through images */
-for(let i=0; i<5; i++){
-    const newImage=document.createElement('img');
-    newImage.setAttribute('src','images/pic'+(i+1)+'.jpg');
+for (let i = 1; i <=5; i++) {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', 'images/pic' + i+ '.jpg');
     thumbBar.appendChild(newImage);
-    newImage.onclick=function(e){
-        displayedImage.src=e.target.src;
+    newImage.onclick = function (e) {
+        displayedImage.src = e.target.src;
     }
 }
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-thumbBar.appendChild(newImage);
-
 /* Wiring up the Darken/Lighten button */
+btn.onclick = function () {
+    const btnClass = btn.getAttribute('class');
+    if (btnClass == 'dark') {
+        btn.setAttribute('class', 'light');
+        btn.textContent = 'Lighten';
+        overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+    } else {
+        btn.setAttribute('class', 'dark');
+        btn.textContent = 'Darken';
+        overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+    }
+}
+
